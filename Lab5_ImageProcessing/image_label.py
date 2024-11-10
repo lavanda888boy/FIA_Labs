5fd5fca8e3c915cfc961ca76c0dc9feeacd7cf60
Sevastian Bajenov <sevabaj@gmail.com>
2024-11-10 19:58:34 +0200

    Add and label images for cnn processing.

---
+++ Lab5_ImageProcessing/image_label.py
@@ -0,0 +1,35 @@
+import os
+import csv
+from PIL import Image
+import matplotlib.pyplot as plt
+
+IMAGES_FOLDER = 'images'
+IMAGE_LABELS = 'labels.csv'
+
+
+def label_images():
+    print("Enter y/n for each image whether it is suitable for passport or not.\n")
+
+    with open(IMAGE_LABELS, mode='w', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerow(['ImageName', 'Label'])
+
+        for image_name in os.listdir(IMAGES_FOLDER):
+            image_path = os.path.join(IMAGES_FOLDER, image_name)
+
+            image = Image.open(image_path)
+
+            plt.imshow(image)
+            plt.axis('off')
+            plt.show(block=False)
+
+            user_input = input(f"{image_name}: ")
+            image_status = True if user_input.lower() == 'y' else False
+
+            writer.writerow([image_name, image_status])
+
+            plt.close()
+
+
+if __name__ == "__main__":
+    label_images()
